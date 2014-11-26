aa4b5c8bb74eac8a5264ba6430f1fb11cd7a05c9
Michel Meyer <mmeyer@trobz.com>
2014-11-26 04:24:57 +0000

    change the loading method for JS/CSS files

--- web_serialized/__openerp__.py
+++ web_serialized/__openerp__.py
@@ -5,6 +5,12 @@
     'category': 'Hidden',
     'description': """
 Add support for fields.serialized in from view with a new widget: "serialized"
+
+Note:
+
+Odoo 8.0 doesn't support `fields.serialized` anymore, you have 
+to install the server wide module `base_field_serialized` 
+from https://github.com/OCA/server-tools OCA repository.
     """,
     'author': 'Trobz',
     'website': 'https://github.com/trobz/openerp-widget-serialized',
@@ -13,27 +19,11 @@ Add support for fields.serialized in from view with a new widget: "serialized"
     ],
     
     'data': [
+        # JS/CSS Assets files
+        'views/web_serialized.xml',
     ],
     
     'qweb' : [
-        'static/src/templates/*'
-    ],
-    
-    
-    'css' : [
-        'static/lib/jsoneditor/jsoneditor.css',
-    
-        'static/src/css/field_serialized.css',
-    ],
-       
-    'js': [
-        # lib
-        'static/lib/jsoneditor/lib/jsonlint/jsonlint.js',
-        'static/lib/jsoneditor/jsoneditor.js',
-        
-        'static/src/js/field_serialized.js'
-    ],
-    
-    'test': [
+        'static/src/xml/*'
     ]
 }
